7b8c13f912ac0f96dfa2ca639154855537841740
Md Ariful Islam <arifulatwork@gmail.com>
2024-02-17 05:39:06 +0600

    check with modal

--- js/main.js
+++ js/main.js
@@ -1,27 +1,48 @@
+// Get the portfolio modal
+var portfolioModal = document.getElementById("portfolio-modal");
 
-    // Get the modal
-    var modal = document.getElementById("portfolio-modal");
+// Get the link that opens the portfolio modal
+var portfolioLink = document.querySelector('a[data-target="portfolio-modal"]');
 
-    // Get the link that opens the modal
-    var portfolioLink = document.querySelector('a[data-target="portfolio-modal"]');
+// Get the <span> element that closes the portfolio modal
+var portfolioCloseBtn = portfolioModal.querySelector(".close");
 
-    // Get the <span> element that closes the modal
-    var span = document.querySelector(".close");
+// When the user clicks the link to open the portfolio modal, open it
+portfolioLink.onclick = function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    portfolioModal.style.display = "block";
+}
 
-    // When the user clicks the link, open the modal
-    portfolioLink.onclick = function(event) {
-        event.preventDefault(); // Prevent default link behavior
-        modal.style.display = "block";
+// When the user clicks on <span> (x) in the portfolio modal, close it
+portfolioCloseBtn.onclick = function() {
+    portfolioModal.style.display = "none";
+}
+
+// Get the resume modal
+var resumeModal = document.getElementById("resume-modal");
+
+// Get the link for the resume
+var resumeLink = document.getElementById("resume-link");
+
+// When the user clicks the resume link, open the resume modal
+resumeLink.onclick = function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    resumeModal.style.display = "block";
+}
+
+// Get the <span> element that closes the resume modal
+var resumeCloseBtn = resumeModal.querySelector(".close");
+
+// When the user clicks on <span> (x) in the resume modal, close the modal
+resumeCloseBtn.onclick = function() {
+    resumeModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the resume modal, close it
+window.onclick = function(event) {
+    if (event.target == resumeModal) {
+        resumeModal.style.display = "none";
+    } else if (event.target == portfolioModal) {
+        portfolioModal.style.display = "none";
     }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+}
